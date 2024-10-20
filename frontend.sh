c38a46caa015ca1a9183d4f651b740f379a2fedc
Shiva <shivanandhan33@gmail.com>
2024-10-20 12:35:28 +0530

    Frontend adding confg file

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,16 @@
+color= "\e[35m"
+no_color= "\e[0m"
+
+echo -e "$color Installing nginx $no_color"
+
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
+echo -e "$color copying conf file $no_color"
 cp nginx.conf /etc/nginx/nginx.conf
+
+echo -e "$color removing file and downloading content $no_color"
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
@@ -10,5 +18,6 @@ cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
 
+echo -e "$color restarting nginx $no_color"
 systemctl enable nginx
 systemctl restart nginx
